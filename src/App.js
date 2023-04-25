2bcbc033828f6b378e11f9a168c354f06d3c271b
knjhw@daum.net <knjhw@daum.net>
2023-04-26 00:20:06 +0900

    movies-demo

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { dummy } from "./movieDummy";
+import Movie from "./components/Movie";
+
+function App() {
+  return (
+    <div>
+      <div className="app-container">
+        {
+          dummy.results.map((item) => {
+            return (
+              <Movie 
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+              />
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
